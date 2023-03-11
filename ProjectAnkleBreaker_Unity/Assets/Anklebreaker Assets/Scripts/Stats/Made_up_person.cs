4154bb56a24d0248e861a1de6b92cb8c5efdea92
Marksman7 <56178417+Marksman7@users.noreply.github.com>
2023-03-10 18:30:07 -0600

    Added_public_bool_to_culate_sussess

--- ProjectAnkleBreaker_Unity/Assets/Anklebreaker Assets/Scripts/Stats/Made_up_person.cs
+++ ProjectAnkleBreaker_Unity/Assets/Anklebreaker Assets/Scripts/Stats/Made_up_person.cs
@@ -6,8 +6,8 @@ public class Made_up_person : Stat_Dict
 {
 
     public string name = "no name";
-    public int[] values = { 0,0,0,0,0,0,0,0};
-
+    public int[] values = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    public int chance_pool = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +21,116 @@ public class Made_up_person : Stat_Dict
         player["deffensive rebound"] = values[5];
         player["assist"] = values[6];
         player["turnover"] = values[7];
+    }
+
+    public string get_name()
+    {
+        return name;
+    }
+
+    public bool dunk_chance()
+    {
+        int rand = Random.Range(0, chance_pool);
+
+
+        if (values[1] < rand)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool three_point_chance()
+    {
+        int rand = Random.Range(0, chance_pool);
+
+
+        if (values[2] < rand)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool free_thorw_chance()
+    {
+        int rand = Random.Range(0, chance_pool);
+
+
+        if (values[3] < rand)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool offensive_rebound_chance()
+    {
+        int rand = Random.Range(0, chance_pool);
+
+
+        if (values[4] < rand)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool deffensive_rebound() //steal_ball
+    {
+        int rand = Random.Range(0, chance_pool);
+
+
+        if (values[5] < rand)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool assist() // passing chance
+    {
+        int rand = Random.Range(0, chance_pool);
+
+
+        if (values[6] < rand)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool turnover() //fumble_ball
+    {
+        int rand = Random.Range(0, chance_pool);
+
+
+        if (values[7] < rand)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     // Update is called once per frame
