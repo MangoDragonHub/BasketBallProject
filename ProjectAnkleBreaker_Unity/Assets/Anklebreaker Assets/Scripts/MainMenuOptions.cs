71fcaf581621289e327a2fdf370b4f3c3ed4e9f6
rzgaming <rashadpatt@gmail.com>
2023-03-13 16:54:52 -0500

    Change MainMenu Options and Added Game Over Screen

--- ProjectAnkleBreaker_Unity/Assets/Anklebreaker Assets/Scripts/MainMenuOptions.cs
+++ ProjectAnkleBreaker_Unity/Assets/Anklebreaker Assets/Scripts/MainMenuOptions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,18 @@ public class MainMenuOptions : MonoBehaviour
 {
     //This houses all the main menu & Pause Menu funtionality
 
-    [SerializeField] string LevelName;
     [SerializeField] GameObject FocusOnButton;
+    [SerializeField] GameObject resetButton;
     [SerializeField] GameObject PauseCanvas;
     [SerializeField] GameObject GameplayCanvas;
+    [SerializeField] GameObject EndGameCanvas;
 
     #region Main Modes
-    public void PlayDemo() 
+    public void LoadLevel(string LevelName) 
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(LevelName);
-    
+        
     }
 
     public void Options() 
@@ -47,6 +50,17 @@ public class MainMenuOptions : MonoBehaviour
 
     }
 
+    public void EndGameScreen() 
+    {
+        Time.timeScale = 0;
+        GameplayCanvas.SetActive(false);
+        PauseCanvas.SetActive(false);
+        EndGameCanvas.SetActive(true);
+        GameObject myEventSystem = GameObject.Find("EventSystem");
+        myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(resetButton);
+
+    }
+
     public void UnPauseGame() 
     {
         Time.timeScale = 1;
@@ -54,6 +68,14 @@ public class MainMenuOptions : MonoBehaviour
         PauseCanvas.SetActive(false);
     }
 
+    
 
     #endregion
+
+
+    public void Start()
+    {
+        GameplayCanvas.SetActive(true);
+        PauseCanvas.SetActive(false);
+    }
 }
