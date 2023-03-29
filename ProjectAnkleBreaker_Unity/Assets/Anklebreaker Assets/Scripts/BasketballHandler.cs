27a538bbc6b3f8b03d2ddfc53f26f2f00669c5a2
Andrew P <74389008+ReviveZygarde@users.noreply.github.com>
2023-03-29 14:26:15 -0500

    Update BasketballHandler.cs

--- ProjectAnkleBreaker_Unity/Assets/Anklebreaker Assets/Scripts/BasketballHandler.cs
+++ ProjectAnkleBreaker_Unity/Assets/Anklebreaker Assets/Scripts/BasketballHandler.cs
@@ -232,7 +232,7 @@ public class BasketballHandler : MonoBehaviour
 
     IEnumerator changeSpamproofBool()
     {
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.2f);
         animAlreadyPlayed = false;
     }
 
