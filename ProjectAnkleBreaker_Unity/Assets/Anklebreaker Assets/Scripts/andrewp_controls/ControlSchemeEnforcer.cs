76402463481618976122afcf588bfefd83fd883a
Andrew P <74389008+ReviveZygarde@users.noreply.github.com>
2023-05-06 14:02:50 -0500

    Update ControlSchemeEnforcer.cs

--- ProjectAnkleBreaker_Unity/Assets/Anklebreaker Assets/Scripts/andrewp_controls/ControlSchemeEnforcer.cs
+++ ProjectAnkleBreaker_Unity/Assets/Anklebreaker Assets/Scripts/andrewp_controls/ControlSchemeEnforcer.cs
@@ -24,7 +24,11 @@ public class ControlSchemeEnforcer : MonoBehaviour
 
     private void EnforceSchemes()
     {
-            player1psm.pl_input.SwitchCurrentControlScheme("Gamepad", InputSystem.GetDevice("XInputControllerWindows"));
-            player2psm.pl_input.SwitchCurrentControlScheme("Gamepad", InputSystem.GetDevice("XInputControllerWindows1"));
+        if (!player1psm.pl_input.enabled || !player2psm.pl_input.enabled)
+        {
+            return; //so null user errors aren't sharted out when you guard or shoot.
+        }
+        player1psm.pl_input.SwitchCurrentControlScheme("Gamepad", InputSystem.GetDevice("XInputControllerWindows"));
+        player2psm.pl_input.SwitchCurrentControlScheme("Gamepad", InputSystem.GetDevice("XInputControllerWindows1"));
     }
 }
